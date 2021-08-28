d8a2f6791e0f33e9d823e38748c9a96757a0d828
Sriranjani315 <84986283+Sriranjani315@users.noreply.github.com>
2021-08-28 13:51:54 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -4,3 +4,11 @@ concatenated_string =str1+str2
 print("concatenated string:",concatenated_string)
 answer=concatenated_string*5
 print(answer)
+
+challenge
+str1 = input("enter the 1st string:\n")
+str2 = input("enter the 2nd string:\n")
+concatenated_string =str1+" "+str2
+print("concatenated string:",concatenated_string)
+ 
+print((concatenated_string+"\n")*5)
