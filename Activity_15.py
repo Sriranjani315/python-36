317cb91eb21d9c2b816478e3abe5190de0087340
Sriranjani315 <84986283+Sriranjani315@users.noreply.github.com>
2021-08-29 22:25:24 +0530

    Update Activity_15.py

--- Activity_15.py
+++ Activity_15.py
@@ -8,16 +8,22 @@ def input_no():
 def volume(l,b,h):    
     k=(l*l)+(b*b)+(h*h)
     volume=((b*b)*(h*h))/(math.sqrt(k))
-    radius= pow((3*float(volume)/(4*(math.pi))),1/3)
-    return volume,radius
+    
+    return volume
+def radius():
+    v=volume(l,b,h)
+    radius= pow((3*v/(4*(math.pi))),1/3)
+    return radius
+    
 def display(volume,radius):    
     print("volume of tromboloid=%0.3f" %(volume))
     print("radius of the sphere=%0.3f"%(radius))
 
 def main():
     l,b,h=input_no()
-    volume_tromboloid,radius=volume(l,b,h)
-    display(volume_tromboloid,radius)
+    volume_tromboloid=volume(l,b,h)
+    Radius=radius()
+    display(volume_tromboloid,Radius)
 
 main()
 
