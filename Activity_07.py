7a059e9994e94507f4df68275f0d34d235023efe
Sriranjani315 <84986283+Sriranjani315@users.noreply.github.com>
2021-08-29 11:17:42 +0530

    Update Activity_07.py

--- Activity_07.py
+++ Activity_07.py
@@ -3,3 +3,11 @@ a=input()
 Mylist=a.split()
 res=int(a[0])+int(a[2])
 print("{0}+{1}={2}".format(a[0],a[2],res))
+# other way using for
+a=input()
+Mylist=a.split()
+sum=0
+for i in range(len(Mylist)):
+    sum=sum+int(Mylist[i])
+    print("{0}+".format(Mylist[i]))
+print("={0}".format(sum))
