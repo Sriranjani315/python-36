54f8af5d037f31bfc8c772750f6fc6813f12b43a
Sriranjani315 <84986283+Sriranjani315@users.noreply.github.com>
2021-08-29 21:07:45 +0530

    Update Activity_14.py

--- Activity_14.py
+++ Activity_14.py
@@ -1,18 +1,19 @@
 #prime or not
+import math
 def input_no():
     num=int(input("Enter the number:"))
     return num
 def prime(n):
-    flag=0
+    flag=True
     for i in range(2,int(math.sqrt(n)+1)):
         if(n%i==0):
-            flag=1
+            flag=false
             break
         else:
             continue
     return flag
 def result(flag):
-    if(flag==0):
+    if(flag==True):
         print("It is a prime number")
     else:
         print("It is not a prime number")
