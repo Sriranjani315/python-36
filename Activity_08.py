eb5e1ad75197416ce875ad69faf13cdf5695adee
Sriranjani315 <84986283+Sriranjani315@users.noreply.github.com>
2021-08-29 13:03:14 +0530

    Update Activity_08.py

---
+++ Activity_08.py
@@ -0,0 +1,41 @@
+https://docs.google.com/document/d/1k7zh6qE_DBLP4FEgf2-Jj5h5dRG6uUA-Ms5J_YddP_8/edit?usp=sharing
+“By the way, the language is named after the BBC show “Monty Python’s Flying Circus” and has nothing to do with reptiles”. 
+								~ python.org
+
+Type your answer for each question here:
+
+What do you understand by the term interpreted language?
+Interpreter language: interpreter language is a programming language that goes through an interpreter which converts code we write into a language understood by your computer's processor.  Executes the instructions directly and freely, without compilation of the program into machine-language instructions.
+
+     
+
+2) What is the difference between an interpreter and a compiler?
+
+DIFFERENCES BETWEEN A COMPILER AND AN INTERPRETER
+
+Compiler: 
+1. It takes the entire code in a go
+2. It generates an intermediate machine code
+3.Its highly used by the production team/industry
+4.Compiler is used by programming languages like C,C++,C#,JAVA,SCALA.,etc
+5.Compiler is faster
+6.More efficient
+7.Size of the compiler is large
+8.Debugging is slow
+
+Interpreter:
+1.It takes single line of the code at a time
+2.It doesnt generate any intermediate machine code
+3.It is generally used by the software development team/environment.
+4.Interpreter is used by programming languages like Python,Ruby,PHP,Perl etc 
+5.Interpreter is slower
+6.Less efficient
+7.Size of the interpreter is small
+8.Debugging is fast
+
+3) In which language is the Python interpreter written in?
+Python interpreter - whose popular/ traditional implementation is called CPython and is written in C.
+Other implementations:
+Iron python (Python running on .NET)
+Jython (Python running on the Java Virtual Machine)
+
