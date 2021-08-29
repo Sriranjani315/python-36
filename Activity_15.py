d024cb6a7e56a09254f6f5b2d6ab41a33a992b39
Sriranjani315 <84986283+Sriranjani315@users.noreply.github.com>
2021-08-29 22:48:59 +0530

    Update Activity_15.py

--- Activity_15.py
+++ Activity_15.py
@@ -4,13 +4,13 @@ def input_no():
     l = float(input("enter the length\n"))
     b = float(input("enter the breadth\n"))
     h = float(input("enter the height\n"))
-    return(l,b,h)
+    return l,b,h
 def volume(l,b,h):    
     k=(l*l)+(b*b)+(h*h)
     volume=((b*b)*(h*h))/(math.sqrt(k))
     
     return volume
-def radius():
+def radius(l,b,h):
     v=volume(l,b,h)
     radius= pow((3*v/(4*(math.pi))),1/3)
     return radius
@@ -22,8 +22,7 @@ def display(volume,radius):
 def main():
     l,b,h=input_no()
     volume_tromboloid=volume(l,b,h)
-    Radius=radius()
+    Radius=radius(l,b,h)
     display(volume_tromboloid,Radius)
 
 main()
-
