fcce78cd4021df28e06c2fa808b5ad1e3ff477e2
Sriranjani315 <84986283+Sriranjani315@users.noreply.github.com>
2021-08-29 20:37:00 +0530

    Update Activity_14.py

--- Activity_14.py
+++ Activity_14.py
@@ -1,27 +1,28 @@
 #prime or not
-def input_no():
-    
-    num=int(input("enter the number\n"))
-    return num
+import math
 
+def input_number():
+    num = int(input("Enter the number\n "))
+    return num
 def prime(n):
-    prime_flag=0
-    if(n>1):
-        import math
-        for i in range(2,int(math.sqrt(n))+1):
+   prime_flag = 0
+   if(n>1):
+       for i in range(2,int(math.sqrt(n))+1):
             if(n%i==0):
                 prime_flag=1
                 break
             return prime_flag
+            
 def display(prime_flag):
     if(prime_flag==0):
-        print("number is prime")
+        print("it is prime")
     else:
-            print("number is not prime")
+        print("it is not prime")
+
 def main():
-    num=input_no()
-    prime_no=prime(num)
-    display(prime_no)
-main()
+    num = input_number()
+    prime_number = prime(num)
+    display(prime_number)
+main()  
             
                       
