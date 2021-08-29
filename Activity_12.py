27eb3adeee0f8198858738ccf9ef43dda8bdeb10
Sriranjani315 <84986283+Sriranjani315@users.noreply.github.com>
2021-08-29 16:46:30 +0530

    Update Activity_12.py

--- Activity_12.py
+++ Activity_12.py
@@ -1,4 +1,10 @@
 #largest of 3 nos
+def input_no():
+    a=int(input("enter a\n"))
+    b=int(input("enter b\n"))
+    c=int(input("enter c\n"))
+    return a,b,c
+
 def compare(a,b,c):
     if (a>=b) and (a>=c): 
         return a
@@ -8,9 +14,7 @@ def compare(a,b,c):
         return c
 
 def main():
-    a=int(input("enter a\n"))
-    b=int(input("enter b\n"))
-    c=int(input("enter c\n"))
+    a,b,c=input_no()
     largest=compare(a,b,c)
     print("largest of all three nos is %d"%(largest))
 main()
